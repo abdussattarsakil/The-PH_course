b9fb40837f2e7eadc8dcf4008d3e6876bd868b8e
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-01-29 20:16:28 +0600

    task 1 done

--- JavaScript Mil=3/module=13/task1.js
+++ JavaScript Mil=3/module=13/task1.js
@@ -1,1 +1,11 @@
-console.log("Node is working!");
+// console.log("Node is working!");
+
+var userCash = 1000;
+var orange = 350;
+var apple = 350;
+
+var fruitCost =orange + apple;
+// console.log(fruitCost);
+
+var returnCash =userCash-fruitCost;
+console.log(returnCash);
