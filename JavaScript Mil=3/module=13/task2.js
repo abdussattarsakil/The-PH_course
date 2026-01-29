d7d0d7532603527e37de891176c8336e73cb621d
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-01-29 20:34:25 +0600

    task 2,3,4 add

---
+++ JavaScript Mil=3/module=13/task2.js
@@ -0,0 +1,12 @@
+var math = 75.25;
+var bio = 65;
+var che = 80;
+var phy = 35.45;
+var bangla = 99.50;
+
+var totalMark =math+bio+phy+che+bangla;
+
+var avarageMark = totalMark/5;
+
+console.log(avarageMark.toFixed(2));
+// doshomik er por 2 digit neoar jonno
