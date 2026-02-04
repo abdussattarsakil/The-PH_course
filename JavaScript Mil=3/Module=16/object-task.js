357f8e88b2e4ad0c96e32a00faf1beebc704f891
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-02-05 02:42:55 +0600

    object task incomplete

---
+++ JavaScript Mil=3/Module=16/object-task.js
@@ -0,0 +1,27 @@
+//Count the number of properties.
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+
+
+//Loop through an object and print the key-value pairs with their types
+
+//output :
+//  key: name | type:  string
+// key: age | type:  number
+// key: city | type:  string
+// key: isStudent | type:  boolean
+
+
+
+let citygen = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
