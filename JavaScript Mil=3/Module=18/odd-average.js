ab654c74bdc4ee455153ff917dbe76c481ae3fd4
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-02-06 03:28:17 +0600

    odd-avarage done

---
+++ JavaScript Mil=3/Module=18/odd-average.js
@@ -0,0 +1,18 @@
+function oddAverage(numbers){
+    let oddCount=0;
+    let oddNumbers=0;
+    let oddArr=[];
+    for(let number of numbers){
+        if(number%2!==0){
+            oddCount++;
+            oddNumbers+=number;
+            oddArr.push(number);
+        }
+    }
+    console.log(numbers);
+    console.log(oddArr);
+    console.log('total odd number ->',oddCount);
+    console.log("avarage of odd number ->" ,oddNumbers)
+}
+oddAverage([4,5,6,1,0,1,5,7,8])
+
