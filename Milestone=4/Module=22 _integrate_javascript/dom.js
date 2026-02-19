9edff69d3e26c2495a971cdfe8233fa458a999c9
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-02-19 22:39:42 +0600

    learn basic dom

---
+++ Milestone=4/Module=22 _integrate_javascript/dom.js
@@ -0,0 +1,12 @@
+let list = document.getElementById("players-container")
+list.style.background="lightCyan";
+
+//create element and set innerText or innerHTML
+const newChild = document.createElement("li");
+newChild.innerText = "new born footballer here";
+
+// find the parent where you will add the child
+const playersList = document.getElementById("players-list");
+
+// append the child to the parent  -append mani add kora
+playersList.appendChild(newChild);
