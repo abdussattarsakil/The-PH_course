608fb6ea28ae4be02da9e69022ac7b3b1b028af5
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-02-20 05:16:41 +0600

    Onclick handler

---
+++ Milestone=4/Module=23/event.js
@@ -0,0 +1,11 @@
+function makeBlack() {
+    document.body.style.backgroundColor = "black";
+}
+
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+function makeGreen() {
+    document.body.style.backgroundColor = 'green'
+}
