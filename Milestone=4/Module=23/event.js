1097483a3b2d917b6b873ccdc31517c763b040df
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-02-20 15:00:58 +0600

    addEventListener learn

--- Milestone=4/Module=23/event.js
+++ Milestone=4/Module=23/event.js
@@ -9,3 +9,33 @@ function makeRed() {
 function makeGreen() {
     document.body.style.backgroundColor = 'green'
 }
+
+let makeBlue = document.getElementById("make-blue");
+makeBlue.onclick = function () {
+    document.body.style.backgroundColor = "blue"
+}
+
+
+
+
+
+//addListener
+//document.getElementById().addEventListener('event-type',handler/function)
+document.getElementById("make-gold").addEventListener("click",
+    function () {
+        document.body.style.backgroundColor = "gold"
+    }
+)
+
+
+let mouseGameBtn= document.getElementById("mouse-game-btn");
+mouseGameBtn.addEventListener("mouseover",
+    function(){
+        document.body.style.backgroundColor="cyan"
+    }
+)
+mouseGameBtn.addEventListener("mouseleave",
+     function(){
+        document.body.style.backgroundColor="salmon"
+    }
+)
