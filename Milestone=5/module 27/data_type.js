3e6662e3362862d0cb1ccbc202c16d70f353c172
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-03-04 13:36:03 +0600

    learn primitive and non primitive data type

---
+++ Milestone=5/module 27/data_type.js
@@ -0,0 +1,6 @@
+// null vs undefined important for interview
+
+//primitive data types (basic data type) :- string,number,boolean,undefined,null
+
+//non primitive data types (reference data type) :- object,array,function
+
