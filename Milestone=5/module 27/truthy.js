b5eb92edf4b2551c097c63b83cbe19a0c2a09b37
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-03-04 14:22:51 +0600

    learn truthy and falsy value

---
+++ Milestone=5/module 27/truthy.js
@@ -0,0 +1,11 @@
+//Falsy value মানে এমন value যাকে JavaScript boolean context এ false হিসেবে ধরে।
+/**  ----------falsy value:----------
+ * false  
+ * 0 
+ * -0
+ * ""   --empty string(" " but space thakle truthy hobe)
+ * null  -no value
+ * undefined - value assign kora hoy ni
+ * NaN -Not a Number(number type এর একটি special value)
+ */
+
