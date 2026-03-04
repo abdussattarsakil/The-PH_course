b47447af916c64b91c2cef942c0a20fd2d29719f
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-03-04 14:41:16 +0600

    learn double equal and tripal equation

---
+++ Milestone=5/module 27/equalSign.js
@@ -0,0 +1,8 @@
+/**
+ * == (Loose Equality)  -Value compare + type conversion
+ * ex-  "5" == 5 → true, [] == false → true
+ * 
+ * 
+ * === (Strict Equality)    -Value + type compare
+ * ex-  "5" === 5 → false, [] === false → false
+ */
