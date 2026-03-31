b244a00f469f7f30e9132cfab5e971759e87cd97
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-03-31 23:45:49 +0600

    learn how to add and get object to local storage

--- localStorage/scripts/local_storage.js
+++ localStorage/scripts/local_storage.js
@@ -1,20 +1,30 @@
-const addNumberToLs=()=>{
-    const number=Math.ceil(Math.random()*100)
+const addNumberToLs = () => {
+    const number = Math.ceil(Math.random() * 100)
     console.log(number);
-    localStorage.setItem('number',number)
+    localStorage.setItem('number', number)
 }
 
-const getNumberFromLS=()=>{
-    let number=localStorage.getItem('number')
-    const div=document.getElementById('numberDiv')
+const getNumberFromLS = () => {
+    let number = localStorage.getItem('number')
+    const div = document.getElementById('numberDiv')
     console.log(div)
-    div.innerHTML=``
-    div.innerText=number
+    div.innerHTML = ``
+    div.innerText = number
 }
 
-const clearDiv=()=>{
-    const div=document.getElementById('numberDiv')
+const clearDiv = () => {
+    const div = document.getElementById('numberDiv')
     console.log(div)
-    div.innerHTML=`No Number Available Now`
+    div.innerHTML = `No Number Available Now`
 }
 
+const customer = { name: "sakil", id: 627, product: "8Gb Ram", price: 3200 }
+
+const addObject = () => {
+    localStorage.setItem("customer", JSON.stringify(customer))
+}
+
+const getObject = () => {
+    const user = JSON.parse(localStorage.getItem("customer"))
+    console.log(user);
+}
