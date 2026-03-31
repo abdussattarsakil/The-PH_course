8378f6c2a4ef0a9e184d6937c0fd196eda858d16
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-03-31 21:05:04 +0600

    fun with local storage

--- localStorage/scripts/local_storage.js
+++ localStorage/scripts/local_storage.js
@@ -4,3 +4,17 @@ const addNumberToLs=()=>{
     localStorage.setItem('number',number)
 }
 
+const getNumberFromLS=()=>{
+    let number=localStorage.getItem('number')
+    const div=document.getElementById('numberDiv')
+    console.log(div)
+    div.innerHTML=``
+    div.innerText=number
+}
+
+const clearDiv=()=>{
+    const div=document.getElementById('numberDiv')
+    console.log(div)
+    div.innerHTML=`No Number Available Now`
+}
+
